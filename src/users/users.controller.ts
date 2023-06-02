b0ab8cab57921b606f26c5419f581b6019f45f69
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-06-02 09:59:22 +0300

    Added password hashing when creating account

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { User } from "./user.model";
 import { UsersService } from "./users.service";
 import { createAccountDTO } from "./dto/createAccountDTO.dto";
@@ -13,4 +13,8 @@ export class UserController {
         return this.userService.createAccount(body)
     }
 
+    @Get(":id")
+    async getInfo(@Param("id") id : number) {
+        return this.userService.getInfo(id)
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt"
 import { createAccountDTO } from "./dto/createAccountDTO.dto";
 import { InjectModel } from "@nestjs/sequelize";
 import { User } from "./user.model";
@@ -9,9 +10,19 @@ export class UsersService {
     constructor(@InjectModel(User) private readonly userModel : typeof User) {}
 
     async createAccount(body: createAccountDTO) {
-       const user = await this.userModel.create({...body})
+       let {username, email, password} = body;
+        password = await bcrypt.hash(password, 10); //sequelize takes object with password field
+       const user = await this.userModel.create({username, email, password})
         user.save()
         return user;
+    }
+
+    async getInfo(id: number) {
+       const user = await this.userModel.findOne({where : {id}}); 
+       if (user) {
+        return user
+       }
+       return {message: "There is no such user"} 
     }
 
     login() {
