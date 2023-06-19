88e0593a27c55452ea0818218a7e2506f9072c9a
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-06-19 18:54:50 +0300

    Tested cascadeDelete and added erorr handling

--- src/cascadeDelete/cascadeDelete.service.ts
+++ src/cascadeDelete/cascadeDelete.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Tags } from "src/tags/schemas/tags.schema";
@@ -16,40 +16,53 @@ export class cascadeDeleteService {
         await this.userModel.deleteOne({_id: id});
         await this.taskModel.deleteMany({user_id: id})
         await this.tagModel.deleteMany({user_id: id})
+        return {message: "Deleted successfully"}
     }
 
     async deleteTask(req: Request, id: string) {
-        const task = await this.taskModel.findOne({_id: id})
-        console.log(task)
-        const user = await this.userModel.findOne({username: req['user'].username});
-        const tags = task.tags;
-        if (user.tasks.includes(task.id)) {
-            try {
+        try {
+            const task = await this.taskModel.findOne({_id: id})
+            const user = await this.userModel.findOne({username: req['user'].username});
+            const tags = task.tags;
+            if (user.tasks.includes(task.id)) {
                 const res = await this.taskModel.deleteOne({_id: id});
                 const result = await this.userModel.updateOne({username: req['user'].username}, {$pull: {tasks: id}})
                 const allResult = await Promise.all(tags.map(async tag => {
                     const tagResult = await this.tagModel.updateOne({_id: tag['_id']}, {$pull: {tasks: id}});
-                    console.log('Update Tag Result:', tagResult);
                     return tagResult;
                 }));
-                return {message: 'Deleted successfully'}
-            } catch(err) {
-                console.log("first")
-                console.log(err)
+                return user.tasks;
+            } else {
+                throw new ForbiddenException()
+            } 
+        } catch(e){ 
+            if (e instanceof(ForbiddenException)){
+                throw e
+            } else {
+                throw new NotFoundException()
             }
         }
-        return {message: 'Deleted unsuccessfully'}
     }
 
     async deleteTag(req: Request, id: string) {
-        const tag = await this.tagModel.findOne({_id: id})
-        const user = await this.userModel.findOne({username: req['user'].username});
-        const tasks = tag.tasks;
-        if (user.tags.includes(tag.id)) {
-            const res = await this.tagModel.deleteOne({_id: id});
-            const userRes = await this.userModel.updateOne({username: req['user'].username}, {$pull: {tags: id}})
-            const allResults = await Promise.all(tasks.map(task => this.taskModel.updateOne({_id: task['_id']}, {$pull: {tags: id}})))
-            return {message: "Deleted successfully"}
+        try {
+            const tag = await this.tagModel.findOne({_id: id})
+            const user = await this.userModel.findOne({username: req['user'].username});
+            const tasks = tag.tasks;
+            if (user.tags.includes(tag.id)) {
+                const res = await this.tagModel.deleteOne({_id: id});
+                const userRes = await this.userModel.updateOne({username: req['user'].username}, {$pull: {tags: id}})
+                const allResults = await Promise.all(tasks.map(task => this.taskModel.updateOne({_id: task['_id']}, {$pull: {tags: id}})))
+                return {message: "Deleted successfully"}
+            } else {
+                throw new ForbiddenException()
+            }
+        } catch(e) {
+            if (e instanceof(ForbiddenException)){
+                throw e;
+            } else {
+                throw new NotFoundException()
+            }
         }
     }
 }
